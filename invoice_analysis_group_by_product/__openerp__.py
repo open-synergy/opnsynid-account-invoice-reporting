3ec1538f3eda26fd248765acbb0785b3b6d6cb1a
Andhitia Rama <andhitia.r@gmail.com>
2017-08-25 21:20:26 +0700

    [8.0.1.0.0] invoice_analysis_group_by_product

---
+++ invoice_analysis_group_by_product/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Analysis Group By Product",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "reports/invoice_report_views.xml",
+    ],
+}
